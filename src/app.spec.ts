bf737eca7c294fa7ea77adef700f2937e232ed2d
Grzegorz Moskal <grzegorz.moskal@tooploox.com>
2017-10-04 18:51:18 +0200

    multiple child rendering support

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -12,17 +12,32 @@ describe("test test", () => {
         root = doc.body.querySelector('div')
     })
 
-    const HELLO_WORLD = "Hello World"
+    const HELLO = "Hello"
+    const WORLD = "world"
     it("renders html tag", () => {
-        const helloWorld = Reactx.createElement('div', null, HELLO_WORLD)
+        const helloWorld = Reactx.createElement('div', null, HELLO)
         ReactxDOM.render(helloWorld, root)
-        expect(root.textContent).toEqual(HELLO_WORLD)
+        expect(root.textContent).toEqual(HELLO)
     })
 
+    it("renders several children in html tag", () => {
+        const div = Reactx.createElement('div', null, HELLO + " ", WORLD)
+        ReactxDOM.render(div, root)
+        expect(root.textContent).toEqual(HELLO + " " + WORLD)
+    })
+
+    const HelloComponent = () => Reactx.createElement('div', null, HELLO)
     it("renders stateless component", () => {
-        const C = () => Reactx.createElement('div', null, HELLO_WORLD)
-        const helloWorld = Reactx.createElement(C, null, null)
-        ReactxDOM.render(helloWorld, root)
-        expect(root.innerHTML).toEqual(`<div>${HELLO_WORLD}</div>`)
+        const HelloComponentWrapper = Reactx.createElement(HelloComponent)
+        ReactxDOM.render(HelloComponentWrapper, root)
+        expect(root.innerHTML).toEqual(`<div>${HELLO}</div>`)
+    })
+
+    it("renders html and stateless child components", () => {
+        const hello = Reactx.createElement(HelloComponent)
+        const world = Reactx.createElement('span', null, WORLD)
+        const wrapper = Reactx.createElement('section', null, hello, world)
+        ReactxDOM.render(wrapper, root)
+        expect(root.innerHTML).toEqual(`<section><div>${HELLO}</div><span>${WORLD}</span></section>`)
     })
 })
--- src/reactx.ts
+++ src/reactx.ts
@@ -1,18 +1,25 @@
-let doc: Document = document
-export function setDocument(d: Document) {
-    doc = d
-}
+let document: Document = window.document
+export const setDocument = (d: Document) => document = d
 
 type Component<T> = (props: T) => HTMLElement
-function createElement<T = void>(tagName: string | Component<T>, props: T, children: any): HTMLElement {
-    console.log(typeof tagName, tagName)
-    if (typeof tagName === 'function') {
-        return tagName(props)
-    }
+type Element<T> = string | Component<T>
+type Children = (string | HTMLElement)[]
 
-    const domElement = doc.createElement(tagName)
-    domElement.innerHTML = children
+function createTagElement<T>(tag: string, children: Children) {
+    const domElement = document.createElement(tag)
+    children.forEach(child => {
+        if (typeof child === 'string')
+            domElement.innerHTML += child
+        else
+            domElement.appendChild(child)
+    })
     return domElement
+}
+
+function createElement<T = void>(e: Element<T>, props: T = null, ...children: Children): HTMLElement {
+    if (typeof e === 'function')
+        return e(props)
+    return createTagElement(e, children)
 }
 
 export const Reactx = {
